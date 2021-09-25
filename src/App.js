029327b1cada7c404d0b5906ce17f0c6aac439df
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-09-25 21:45:08 +0200

    create PricingContainer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import Pricing from "./components/Pricing";
+
+const PricingContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #e5e5e5;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <PricingContainer>
+      <Pricing />
+    </PricingContainer>
+  );
 }
 
 export default App;
