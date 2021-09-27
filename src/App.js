c2bfb766265c2f6a45b0f35a7f988d3b95e06f7c
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-09-27 16:16:46 +0200

    add PageHeader placeholder

--- src/App.js
+++ src/App.js
@@ -11,55 +11,65 @@ const PricingContainer = styled.div`
   justify-content: space-around;
 `;
 
+const PageHeader = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: red;
+`;
+
 function App() {
   return (
-    <PricingContainer>
-      <Pricing
-        headerText="Essential"
-        descriptionText="Understand the true pulse of your organization with regular Check-ins and guided 1-on-1s, and show meaningful appreciation when it matters most using High Fives."
-        value="8€"
-        duration="per month"
-        text="per employee"
-        buttonText="Select Plan"
-        list={["Core HR", "Payroll", "Recruiting", "Employee HUB"]}
-        info={[]}
-      />
-      <Pricing
-        headerText="Growth"
-        descriptionText="Understand the true pulse of your organization with regular Check-ins and guided 1-on-1s, and show meaningful appreciation when it matters most using High Fives."
-        value="14€"
-        duration="per month"
-        text="per employee"
-        buttonText="Select Plan"
-        info={[
-          "Core HR",
-          "Payroll",
-          "Recruiting",
-          "Employee HUB",
-          "Performance management",
-          "Shift planning",
-        ]}
-        list={[]}
-      />
-      <Pricing
-        headerText="Performance"
-        descriptionText="Understand the true pulse of your organization with regular Check-ins and guided 1-on-1s, and show meaningful appreciation when it matters most using High Fives."
-        value="20€"
-        duration="per month"
-        text="per employee"
-        buttonText="Select Plan"
-        info={[
-          "Core HR",
-          "Payroll",
-          "Recruiting",
-          "Employee HUB",
-          "Performance management",
-          "Shift planning",
-          "Componsation management",
-        ]}
-        list={[]}
-      />
-    </PricingContainer>
+    <>
+      <PageHeader />
+
+      <PricingContainer>
+        <Pricing
+          headerText="Essential"
+          descriptionText="Understand the true pulse of your organization with regular Check-ins and guided 1-on-1s, and show meaningful appreciation when it matters most using High Fives."
+          value="8€"
+          duration="per month"
+          text="per employee"
+          buttonText="Select Plan"
+          list={["Core HR", "Payroll", "Recruiting", "Employee HUB"]}
+          info={[]}
+        />
+        <Pricing
+          headerText="Growth"
+          descriptionText="Understand the true pulse of your organization with regular Check-ins and guided 1-on-1s, and show meaningful appreciation when it matters most using High Fives."
+          value="14€"
+          duration="per month"
+          text="per employee"
+          buttonText="Select Plan"
+          info={[
+            "Core HR",
+            "Payroll",
+            "Recruiting",
+            "Employee HUB",
+            "Performance management",
+            "Shift planning",
+          ]}
+          list={[]}
+        />
+        <Pricing
+          headerText="Performance"
+          descriptionText="Understand the true pulse of your organization with regular Check-ins and guided 1-on-1s, and show meaningful appreciation when it matters most using High Fives."
+          value="20€"
+          duration="per month"
+          text="per employee"
+          buttonText="Select Plan"
+          info={[
+            "Core HR",
+            "Payroll",
+            "Recruiting",
+            "Employee HUB",
+            "Performance management",
+            "Shift planning",
+            "Componsation management",
+          ]}
+          list={[]}
+        />
+      </PricingContainer>
+    </>
   );
 }
 
