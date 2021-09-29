8308ff39ee77f474bd67f0ae6a043c3ae7cd6764
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-09-29 11:45:42 +0200

    merge PageHeader component to App

--- src/App.js
+++ src/App.js
@@ -17,10 +17,63 @@ const PricingContainer = styled.div`
   margin: 0 auto 200px;
 `;
 
+const PageHeaderComponent = styled.div`
+  width: 100%;
+  height: 200px;
+  background: linear-gradient(
+    360deg,
+    rgba(255, 255, 255, 0.056) 0%,
+    rgba(37, 67, 136, 0.1) 42.16%,
+    rgba(107, 180, 221, 0) 92.38%
+  );
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 40px;
+  line-height: 45px;
+  font-weight: 700;
+  color: #1f2643;
+  margin-bottom: 24px;
+`;
+
+const SubDurationContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SubDuration = styled.span`
+  font-size: 15px;
+  line-height: 17.66px;
+  color: #1f2643;
+  font-weight: 400;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+const SmallForwardSlash = styled.div`
+  width: 3.52px;
+  height: 28.51px;
+  background-color: #ced1d9;
+  border-radius: 100px;
+  transform: rotate(20deg);
+  /* margin: 0 20px; */
+`;
+
 function App() {
   return (
     <>
-      <PageHeader pageTitle="Base plan" />
+      <PageHeaderComponent>
+        <Title>Base plan</Title>
+        <SubDurationContainer>
+          <SubDuration>Monthly</SubDuration>
+          <SmallForwardSlash />
+          <SubDuration>Yearly</SubDuration>
+        </SubDurationContainer>
+      </PageHeaderComponent>
 
       <PricingContainer>
         <Pricing
